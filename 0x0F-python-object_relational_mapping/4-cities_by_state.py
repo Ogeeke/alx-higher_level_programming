51ece8a144deac67394f8f7d95a752d62bada675
ogeeke <ogechieke2014@gmail.com>
2024-04-11 15:56:06 -0700

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
-# Lists all cities of the database hbtn_0e_4_usa, ordered by city id.
+''' This module is a script to connect to a MySQL database'''
 
-import sys
-import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT `c`.`id`, `c`.`name`, `s`.`name` \
-                 FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    [print(city) for city in c.fetchall()]
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    if (len(sys.argv) == 4):
+
+        db = (MySQLdb.connect(host='localhost', user=sys.argv[1],
+              passwd=sys.argv[2], db=sys.argv[3], port=3306))
+
+        cur = db.cursor()
+        cur.execute('SELECT cities.id, cities.name, states.name FROM\
+                     cities, states WHERE cities.state_id=states.id')
+
+        rows = cur.fetchall()
+
+        for row in rows:
+            print(row)
+
+        cur.close()
+        db.close()
