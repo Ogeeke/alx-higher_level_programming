7accc3c2643d01a0079de56e1634206ec8ccde05
ogeeke <ogechieke2014@gmail.com>
2024-04-11 16:00:12 -0700

    5-filter_cities.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,16 +1,24 @@
 #!/usr/bin/python3
-# Displays all cities of a given state from the
-# states table of the database hbtn_0e_4_usa.
-# Safe from SQL injections.
+''' This module is a script to connect to a MySQL database'''
 
-import sys
-import MySQLdb
 
-if __name__ == "__main__":
-    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
-    c = db.cursor()
-    c.execute("SELECT * FROM `cities` as `c` \
-                INNER JOIN `states` as `s` \
-                   ON `c`.`state_id` = `s`.`id` \
-                ORDER BY `c`.`id`")
-    print(", ".join([ct[2] for ct in c.fetchall() if ct[4] == sys.argv[4]]))
+if __name__ == '__main__':
+    import MySQLdb
+    import sys
+
+    if (len(sys.argv) == 5):
+
+        db = (MySQLdb.connect(host='localhost', user=sys.argv[1],
+              passwd=sys.argv[2], db=sys.argv[3], port=3306))
+
+        cur = db.cursor()
+        cur.execute('SELECT cities.name FROM cities INNER JOIN\
+                     states ON cities.state_id=states.id\
+                     WHERE states.name=%s', (sys.argv[4],))
+
+        rows = cur.fetchall()
+        values = [row[0] for row in rows]
+        print(*values, sep=', ')
+
+        cur.close()
+        db.close()
